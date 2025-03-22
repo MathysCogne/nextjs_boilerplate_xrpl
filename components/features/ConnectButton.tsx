662f6277a2113a86e509c5b1377334ad71daf4e3
Mathys Cogne <mathys.cognef@gmail.com>
2025-03-22 21:24:41 +0100

    Init

---
+++ components/features/ConnectButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { TEXT } from '@/constants/text';
+import { Button } from '@/components';
+
+export const ConnectButton = () => {
+  return (
+    <>
+    </>
+  );
+};
---
+++ components/index.ts
@@ -0,0 +1,13 @@
+// Layout
+export { NavBar } from './layout/NavBar';
+export { Footer } from './layout/Footer';
+
+// Features
+export { ConnectButton } from './features/ConnectButton';
+
+// Sections
+export { Hero } from './section/Hero';
+
+// UI
+export { StarryBackground } from './ui/StarryBackground';
+export { Button } from './ui/Button';
